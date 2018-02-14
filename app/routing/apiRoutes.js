672905bd82382a44b8a3dd70bc7006a86d385253
venu.mora <venukumar.mora@gmail.com>
2018-02-14 00:44:06 -0800

    Complete Implementation

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,9 @@
 const { friendFinderDataBase } = require('../data/friends');
+const sum = (accumulator, currentValue) => accumulator + currentValue;
+Array.prototype.diff = function(inputArray) {
+    return this.map(function(item, index) { return  Math.abs(item - inputArray[index]); });
+};
+
 const apiRouters = function(app) {
     this.app = app;
     this.friendFinderDataBase = new friendFinderDataBase();
@@ -8,8 +13,32 @@ const apiRouters = function(app) {
             res.json(this.friendFinderDataBase.friends);
         }.bind(this));
 
-        this.app.post('/api/friends', function(req, res) {
-        });
+        this.app.post('/api/friends', function(req, res, next) {
+            const data = Object.assign({}, req.body);
+            data.score = data.scores.map(function(score){
+                return score ? parseInt(score, 10) : 0;
+            });
+            let closeMatch = null;
+            let lastHighScore;
+
+            this.friendFinderDataBase.friends.forEach(friend => {
+                let matchingScore = 0;
+                let matchingScoreArray = data.score.diff(friend.scores);
+                matchingScore = matchingScoreArray.reduce(sum);
+                if(lastHighScore === undefined) {
+                    lastHighScore = matchingScore;
+                }
+
+                if(lastHighScore >= matchingScore) {
+                    closeMatch = friend;
+                    lastHighScore = matchingScore;
+                }
+            });
+            
+            this.friendFinderDataBase.friends.push(data);
+            const redirectUrl = `/survey${closeMatch ? `?matchName=${closeMatch.name}&matchImg=${closeMatch.photo}` : ''}`;
+            res.redirect(redirectUrl);
+        }.bind(this));
     };
 };
 
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,6 +8,9 @@ const htmlRouters = function(app) {
         this.app.get('/survey', function(req, res) {
             res.sendFile(path.join(__dirname, '../../app/public/survey.html'));
         });
+        this.app.get('/choosenfiles/:fileName', function(req, res) {
+            res.sendFile(path.join(__dirname, '../../node_modules/chosen-js/' + req.params.fileName));
+        });
     };
 };
 
