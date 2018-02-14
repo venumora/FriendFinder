9dbdc45c4a3ed51f521597e3cbe0069e1ce3e950
venu.mora <venukumar.mora@gmail.com>
2018-02-14 00:54:37 -0800

    Complete Implementation

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,23 +1,21 @@
 const friendFinderDataBase = function(app) {
-    this.friends = [];
-    this.friends.push({
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-          ]
+  this.friends = [];
+  this.friends.push(
+      {
+        'name': 'Ahmed',
+        'photo': 'https://i.ytimg.com/vi/rXMIQDVOs98/hqdefault.jpg',
+        'scores': [5, 5, 4, 4, 3, 3, 2, 2, 1, 1]
+      },
+      {
+        'name': 'John',
+        'photo': 'http://moziru.com/images/cartoon-network-clipart-cartoon-character-14.jpg',
+        'scores': [1, 1, 2, 2, 3, 4, 4, 4, 5, 5]
+      },
+      {
+        'name': 'Ramu',
+        'photo': 'http://www.whoa.in/201604-Whoa/Lord-Rama-Art-Image-for-Shree-Ram-Lovers.jpg',
+        'scores': [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
       });
 };
 
-module.exports = {
-    friendFinderDataBase
-};
+module.exports = {friendFinderDataBase};
